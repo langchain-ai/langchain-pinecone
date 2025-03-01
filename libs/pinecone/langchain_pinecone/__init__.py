3a9ca7ec21994a25f4801f52adcb317745880fed
Sifat Hasan <sihabhossan633@gmail.com>
2025-03-01 14:33:54 +0600

    Update __init__.py

--- libs/pinecone/langchain_pinecone/__init__.py
+++ libs/pinecone/langchain_pinecone/__init__.py
@@ -1,6 +1,6 @@
 from langchain_pinecone.embeddings import PineconeEmbeddings
 from langchain_pinecone.vectorstores import Pinecone, PineconeVectorStore
-from langchain_community.document_compressors.pinecone_rerank import PineconeRerank
+from langchain_pinecone.pinecone_rerank import PineconeRerank
 
 __all__ = [
     "PineconeEmbeddings",
